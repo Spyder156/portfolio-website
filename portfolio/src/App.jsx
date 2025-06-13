fcb75575fee85af5e49414467dac42527172b847
Raghav Suryan <suryanraghav@gmail.com>
2025-06-14 04:55:51 +0530

    Basic Structure

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <h1>Hello, React!</h1>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/projects">Projects</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+
+        <hr />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- portfolio/src/pages/Projects.jsx
+++ portfolio/src/pages/Projects.jsx
@@ -1,1 +1,11 @@
- 
+import React from 'react';
+
+function Projects() {
+  return (
+    <div>
+      <h1>Projects Page</h1>
+    </div>
+  );
+}
+
+export default Projects; 
